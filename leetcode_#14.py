6bf21fb2c858da716cc52ac49fa84d5950b24834
minseok <kmus1232@gmail.com>
2022-01-03 22:45:46 +0900

    solution of leetcode prob 14

---
+++ leetcode_#14.py
@@ -0,0 +1,16 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        shortest = min([len(word) for word in strs])
+        ans = ""
+        for i in range(shortest):
+            cur = strs[0][i]
+            break_flag = False
+            for word in strs:
+                if cur != word[i]:
+                    break_flag = True
+                    break
+            if break_flag == True:
+                break
+            ans += cur
+        return ans
+            
