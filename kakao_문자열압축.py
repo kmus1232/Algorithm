60bfe89f6acbc97e8eab31e104c24143e488e919
minseok <kmus1232@gmail.com>
2022-01-11 20:34:34 +0900

    improved

--- kakao_문자열압축.py
+++ kakao_문자열압축.py
@@ -1,20 +1,23 @@
 def solution(s):
     ans = len(s)
     for l in range(1, len(s) // 2 + 1):
-        words = [s[i: i + l] for i in range(0, len(s), l)]
-        cmp_words = words[1:] + ['']
-        res = ''
-        cnt = 1
-        for i in range(len(words)):
-            if words[i] == cmp_words[i]:
-                cnt += 1
-            else:
-                if cnt == 1:
-                    res += words[i]
-                else:
-                    res += str(cnt) + words[i]
-                    cnt = 1
-        ans = min(ans, len(res))
+        compress_result = compress(s, l)
+        ans = min(ans, compress_result)
     return ans
 
-print(solution("ababcdcdababcdcd"))
+
+def compress(s: str, l: int) -> str:
+    words = [s[i: i + l] for i in range(0, len(s), l)]
+    cmp_words = words[1:] + ['']
+    res = ''
+    cnt = 1
+    for i in range(len(words)):
+        if words[i] == cmp_words[i]:
+            cnt += 1
+        else:
+            if cnt == 1:
+                res += words[i]
+            else:
+                res += str(cnt) + words[i]
+                cnt = 1
+    return res
