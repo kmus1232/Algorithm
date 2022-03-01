6905bf9792380c6b7db01afe262a6c316e4a7f57
kim_minseok <kmus1232@gmail.com>
2022-03-01 15:28:05 +0900

    220301

---
+++ kakao_가사검색_bisect.py
@@ -0,0 +1,28 @@
+from bisect import bisect, insort
+from collections import defaultdict
+
+
+def find_query(words, query) -> int:
+    start = end = ""
+    for c in query:
+        start += c if c != '?' else 'a'
+        end += c if c != '?' else 'z'
+    start_idx = bisect(words, start)
+    end_idx = bisect(words, end)
+    return end_idx - start_idx
+
+
+def solution(words, queries):
+    word_dict = defaultdict(list)
+    reversed_word_dict = defaultdict(list)
+    for word in words:
+        insort(word_dict[len(word)], word)
+        insort(reversed_word_dict[len(word)], word[::-1])
+
+    result = []
+    for q in queries:
+        if q[0] == '?':
+            result.append(find_query(reversed_word_dict[len(q)], q[::-1]))
+        else:
+            result.append(find_query(word_dict[len(q)], q))
+    return result
