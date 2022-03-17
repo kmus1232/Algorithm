5a03bb08d3bedc3c805ce90aa24ab03244cfe450
kim_minseok <kmus1232@gmail.com>
2022-03-17 16:04:43 +0900

    .

--- boj_9095.py
+++ boj_9095.py
@@ -1,1 +1,14 @@
+arr = []
 n = int(input())
+
+for _ in range(n):
+    arr.append(int(input()))
+
+max_n = max(arr)
+dp = [0 for _ in range(max_n + 1)]
+dp[1], dp[2], dp[3] = 1, 2, 4
+for i in range(4, max_n + 1):
+    dp[i] = dp[i-1] + dp[i-2] + dp[i-3]
+
+for e in arr:
+    print(dp[e])
