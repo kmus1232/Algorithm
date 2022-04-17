cd55d0c2df7135483ffc3e51632fb46d92a1e568
kim_minseok <kmus1232@gmail.com>
2022-04-17 16:15:20 +0900

    .

--- combination_dfs.py
+++ combination_dfs.py
@@ -1,17 +1,13 @@
 n, m = map(int, input().split())
 
 
-def dfs(last, cnt, arr):
+def dfs(s, cnt, arr):
     if cnt == m:
-        for e in arr:
-            print(e, end=' ')
-        print()
+        print(arr)
         return
 
-    for i in range(last, n + 1):
-        arr.append(i)
-        dfs(i + 1, cnt + 1, arr)
-        arr.pop()
+    for i in range(s, n + 1):
+        dfs(i + 1, cnt + 1, arr + [i])
 
 
 dfs(1, 0, [])
