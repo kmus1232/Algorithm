0b23e5726e5cdd877c6d6e92e85967e5e64edd7d
kim_minseok <kmus1232@gmail.com>
2023-01-05 12:15:49 +0900

    .

---
+++ java/algorithm/src/main/java/leetcode_316.java
@@ -0,0 +1,13 @@
+class leetcode_316 {
+
+    public static void main(String[] args) {
+        StringBuffer sb = new StringBuffer();
+        sb.append(1123).append("hello");
+        System.out.println(sb.toString());
+
+    }
+
+//    public String removeDuplicateLetters(String s) {
+//
+//    }
+}
--- python/test.py
+++ python/test.py
@@ -1,32 +1,22 @@
-print("강의실 배정 프로그램을 시작합니다.")
-print("가능한 많은 예약을 받기 위해 취소되는 예약이 생길 수 있습니다.\n")
+from typing import *
+import collections
 
-q = []
-while True:
-    print("예약하고자 하는 시간을 입력해주세요")
-    print("시작 시간 : ", end='')
-    start = int(input())
-    print("종료 시간 : ", end='')
-    end = int(input())
 
-    if start >= end:
-        print("잘못된 입력입니다.")
-        continue
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
-    q.append((start, end))
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
-    print("예약 입력을 계속 하시겠습니까? (Y/N) : ", end='')
-    select = input()
-    if select in {'N', 'n'}:
-        break
+class Solution:
+    def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        h1, h2 = l1, l2
 
-q.sort(key=lambda a: (a[1], a[0]), reverse=True)  # 끝나는 시간 순으로 오름차순 정렬
-result = [q.pop()]
-while q:
-    if q[-1][0] >= result[-1][1]:
-        result.append(q.pop())
-    else:
-        q.pop()
 
-print("\n확정된 예약 시간표입니다.")
-print(result)
+if __name__ == "__main__":
+    sol = Solution()
